5fdb46da25c5f0c4d1b0315eb112822a04065ae7
unknown <Dell@RN-BLR-LT-18.Fugo001.net>
2023-10-05 12:27:43 +0530

    build error fixed

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -43,7 +43,7 @@ export default function DashboardPage() {
           <div className="flex items-center justify-between space-y-2">
             <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
             <div className="flex items-center space-x-2">
-              <CalendarDateRangePicker />
+              {/* <CalendarDateRangePicker /> */}
               <Button>Download</Button>
             </div>
           </div>
