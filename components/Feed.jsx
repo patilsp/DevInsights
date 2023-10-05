91aabb7e6e59e5a1b8d7301ec71e5fff69793670
unknown <Dell@RN-BLR-LT-18.Fugo001.net>
2023-10-05 17:21:56 +0530

    build error fixed

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -121,7 +121,7 @@ const Feed = () => {
                 value={searchText}
                 onChange={handleSearchChange}
                 required
-                className="relative inline-flex h-9 w-full items-center justify-start rounded-md border border-input bg-transparent px-4 py-2 text-sm font-medium text-muted-foreground shadow-sm transition-colors hover:bg-accent hover:text-accent-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 sm:pr-12 md:w-40 lg:w-64"
+                className="relative inline-flex h-9 w-full items-center justify-start rounded-md border border-input bg-transparent pl-2 py-2 text-sm font-medium text-muted-foreground shadow-sm transition-colors hover:bg-accent hover:text-accent-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 sm:pr-12 md:w-40 lg:w-64"
               />
              <kbd className="pointer-events-none absolute right-1.5 top-1.5 hidden h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium opacity-100 sm:flex">
               <span className="p-1 text-xs">⌘</span>S
